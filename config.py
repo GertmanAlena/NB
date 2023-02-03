1fd1bfb5cdad5878a81fd903db10a4216603842b
Alena <gertmanea@mail.ru>
2023-02-03 20:26:21 +0300

    "03/02/2022"

--- config.py
+++
@@ -1,9 +0,0 @@
-TOKEN = "5959879495:AAHS_2oTsVNoxKZ5YViCO6PSbi2h-IzpZIg"
-
-db_config = {
-    "mysql": {
-        "host": "localhost:3306",
-        "user": "root",
-        "pass": "A12D16E13A19/4",
-    }
-}
