a4d45df5ead91d6fb710512938b6a4da6a3c8ace
Alena <gertmanea@mail.ru>
2023-02-03 23:28:09 +0300

    "доделала меню info"

--- config.py
+++
@@ -1,9 +0,0 @@
-TOKEN = "5959879495:AAFelVOVMc5xSLkNX4xRJgFLyK0GuSrjlxk"
-
-db_config = {
-    "mysql": {
-        "host": "localhost:3306",
-        "user": "root",
-        "pass": "A12D16E13A19/4",
-    }
-}
