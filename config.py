83c1d3e909348de907e9c29ad9f8e65c54af00bb
Alena <gertmanea@mail.ru>
2023-02-05 12:16:27 +0300

    ".gitignor"

--- config.py
+++
@@ -1,9 +0,0 @@
-TOKEN = "5959879495:AAHTzGC14S1AxMqHfVVi1VGmsgupJ9Aq7UI"
-
-db_config = {
-    "mysql": {
-        "host": "localhost:3306",
-        "user": "root",
-        "pass": "A12D16E13A19/4",
-    }
-}
