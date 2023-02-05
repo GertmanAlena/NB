cd5f8fe2538dad4f6bd8509a04b0b06abc4a31cb
Alena <gertmanea@mail.ru>
2023-02-05 11:43:45 +0300

    Start ignoring config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-TOKEN = "5959879495:AAFByHWP96XkLkrfH9YyZF5STCG0lE07nT8"
-
-db_config = {
-    "mysql": {
-        "host": "localhost:3306",
-        "user": "root",
-        "pass": "A12D16E13A19/4",
-    }
-}
